f8aac26fe16544ca0656804f6a9fcffd5eac0fad
ArTinker54 <19artem191@gmail.com>
2026-05-25 21:32:44 +0300

    chore(cms): диагностический пробник + страховка session_set_cookie_params для старого PHP

--- public/cms/functions.php
+++ public/cms/functions.php
@@ -5,7 +5,9 @@
  */
 
 if (session_status() === PHP_SESSION_NONE) {
-  session_set_cookie_params(['httponly' => true, 'samesite' => 'Lax']);
+  if (PHP_VERSION_ID >= 70300) {
+    session_set_cookie_params(['httponly' => true, 'samesite' => 'Lax']);
+  }
   session_start();
 }
 
